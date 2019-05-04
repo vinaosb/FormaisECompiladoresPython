58c0b78002e2863efc085107d403e02dc3fc8a9e
Bruno George de Moraes <brunogm0@gmail.com>
2019-05-04 18:06:33 -0300

    tabulation and save values as dictionary
    v0. GUI

--- src/main_simpleui.py
+++ src/main_simpleui.py
@@ -4,26 +4,40 @@ import sys
 import PySimpleGUI as sg  
 
 sg.ChangeLookAndFeel('BlueMono')
-
-tab1_layout =  [[sg.T('This is inside tab 1')]]        
-tab2_layout =  [[sg.T('AF')], [sg.T('ER')], [sg.T('GR')],   
-               [sg.In(key='in')]]    
+sg.SetOptions(text_justification='right')      
 
 
 
-# Column layout
-col = [[sg.Text('Entrada', text_color='white', background_color='blue')],
-        [sg.Text('ER', text_color='white', background_color='blue'), sg.Input('col input 1')],
-        [sg.Text('ER', text_color='white', background_color='blue'), sg.Input('col input 2')],
-        [sg.Text('GR', text_color='white', background_color='blue'), sg.Input('col input 3')]]
+tab1_layout =  [[sg.Text('ER', text_color='white', background_color='gray'), sg.Input(do_not_clear=True, key='_ER_')],
+        [sg.Text('AF', text_color='white', background_color='gray'), sg.Input(do_not_clear=True, key='_AF_')],        
+        [sg.Text('GR', text_color='white', background_color='gray'), sg.Multiline( size=(42, 11), key='_GR_')],
+        [sg.Submit(), sg.Cancel()]]
+
+tab2_layout = [
+               [sg.In(key='_in2_')]]
+tab3_layout = [
+               [sg.In(key='_in2_')]]
+
+
 # Window layout
-layout = [[sg.Listbox(values=('Listbox Item 1', 'Listbox Item 2', 'Listbox Item 3'),
-                      select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, size=(20, 3)),
-           sg.Column(col, background_color='blue')],
-          [sg.Input('Last input')],
-          [sg.OK()]]
+layout = [
+         [sg.TabGroup([[sg.Tab('Entrada', tab1_layout),
+            sg.Tab('Salvo', tab2_layout), sg.Tab('Conversoes', tab3_layout)]])],  
+         ]
 
 # Display the window and get values
+#    while True:
 event, values = sg.Window(' Trabalho Formais by Bruno e Vinicius - 2019.1 ').Layout(layout).Read()
+#    if event == 'EXIT'  or event is None:      
+#        break # exit button clicked
+#    if event == 'script1':      
+#    ExecuteCommandSubprocess('pip', 'list')      
+#    elif event == 'script2':      
+#    ExecuteCommandSubprocess('python', '--version')      
+#   elif event == 'Run':      
+#    ExecuteCommandSubprocess(value[0])      
 
-sg.Popup(event, values, line_width=200)
+# salvar editar:  valores recebidos como dicionario
+print(event, values['_ER_'], values['_AF_'], values['_GR_']) 
+
+
