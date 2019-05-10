9890752c05c08d32c96b14da11d16ee72ed6c50e
Bruno George de Moraes <brunogm0@gmail.com>
2019-05-10 11:37:52 -0300

    gui+++  table  + make

--- src/main_simpleui.py
+++ src/main_simpleui.py
@@ -10,32 +10,27 @@ import PySimpleGUI as sg
 sg.ChangeLookAndFeel('BlueMono')
 sg.SetOptions(text_justification='right')      
 
+columm_layout = [[]]
+MAX_ROWS = 8
+MAX_COL = 7
+for i in range(MAX_ROWS):
+   inputs = [sg.T('{}'.format(i), size=(4,1), justification='right')] + [sg.In(size=(10, 1), pad=(1, 1), justification='right', key=(i,j), do_not_clear=True) for j in range(MAX_COL)]
+   columm_layout.append(inputs)
 
-
-def table():
-   columm_layout = [[]]
-   MAX_ROWS = 20
-   MAX_COL = 10
-   for i in range(MAX_ROWS):
-        inputs = [sg.T('{}'.format(i), size=(4,1), justification='right')] + [sg.In(size=(10, 1), pad=(1, 1), justification='right', key=(i,j), do_not_clear=True) for j in range0(MAX_COL)]
-        columm_layout.append(inputs)
-
-
-## def afTable         
-##        [sg.Text('AF - Digite linha , coluna e valor:', text_color='white', background_color='gray'),        
-##            sg.In(key='Linha de entrada', justification='right', size=(8,1), pad=(1,1), do_not_clear=True),
-##            sg.In(key='Coluna de entrada', size=(8,1), pad=(1,1), justification='right', do_not_clear=True),
-##            sg.In(key='valor', size=(8,1), pad=(1,1), justification='right', do_not_clear=True)],
-##        #
-##         [sg.Column(columm_layout, size=(800,600), scrollable=True)]]
-##        ]]#, key='_AF_'
 
 menu_def = [['Info', 'Sobre...'],]
 
 tab1_layout = [ [sg.Text('ER', text_color='white', background_color='gray'), sg.Input(do_not_clear=True, key='_ER_')],
         [sg.ReadFormButton('SubmitER', button_color=('gray34','azure2'), key='submit')],
         [sg.Text('GR', text_color='white', background_color='gray'), sg.Multiline( size=(42, 11), key='_GR_')],
-        [sg.ReadFormButton('SubmitGR', button_color=('gray34','azure2'), key='submit')] ]
+        [sg.ReadFormButton('SubmitGR', button_color=('gray34','azure2'), key='submit')],
+
+                
+        [sg.Text('AF', text_color='white', background_color='gray'), sg.Text('Digite linha , coluna:', text_color='white', background_color='gray'),        
+            sg.In(key='inROW', justification='right', size=(8,1), pad=(1,1), do_not_clear=True),
+            sg.In(key='inCOL', size=(8,1), pad=(1,1), justification='right', do_not_clear=True)],
+        [sg.Column(columm_layout, size=(604,212),  key='_AF_', scrollable=True)],
+        [sg.ReadFormButton('SubmitAF', button_color=('gray34','azure2'))] ]
 
 tab2_layout = [ [sg.T('Carregar ER, AF, GR')],
               [sg.Button('Open')],
@@ -66,11 +61,9 @@ while True:
         expressoes = crud.load_expressoes()
     elif event == 'Save':
         ##
-        values['_ER_'] 
-        crud.save_expressoes( expressoes )
-        
- 
+         ext = expressao_regular()
 
+         
 # valores recebidos como dicionario
 #print(event, values['_ER_'], values['_AF_'], values['_GR_']) 
 
